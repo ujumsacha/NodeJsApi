593d77ea41933d25d2943eba0fe7181dab0a9415
maxime Sacha <maxime.ankon@gmail.com>
2021-10-02 23:03:48 +0000

    Update Index to 3000 port
    Co-Authored-By: ujumsacha <45185850+ujumsacha@users.noreply.github.com>

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 require("./app/routes/product")(app);
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
