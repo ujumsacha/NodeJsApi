6dc651709b699d273309e934899c34a1e04464c0
maxime Sacha <maxime.ankon@gmail.com>
2021-10-04 00:14:31 +0000

    last commit

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 const express = require("express");
-// const cors = require("cors");
+
 
 const app = express();
-
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-
-// app.use(cors(corsOptions));
-
 
 app.use(express.json());
 
 
 app.use(express.urlencoded({ extended: true }));
-//sequelize
+
 const db = require("./app/models");
 db.sequelize.sync();
-// simple route
+
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenue" });
 });
 
-// set port, listen for requests
+
 require("./app/routes/product")(app);
 
 const PORT = process.env.PORT || 3000;
